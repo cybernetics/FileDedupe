f308446a51fbce3fa033ef0341b496d2602652cc
Andrew Binstock <abinstock@pacificdataworks.com>
2017-07-16 17:04:40 -0700

    -Added usage information -Moved copyright notice to separate method. -Tightened up some remaining code

--- src/filededup/Main.java
+++ src/filededup/Main.java
@@ -2,13 +2,11 @@
  * Looks for duplicate files based on CRC-32 checksumming. 
  * Project requires JDK 8 or later.
  *
- * Copyright (c) 2015 by Andrew Binstock. All rights reserved.
+ * Copyright (c) 2015-7 by Andrew Binstock. All rights reserved.
  * Licensed under the Creative Commons Attribution, Share Alike license
  * (CC BY-SA). Consult: https://creativecommons.org/licenses/by-sa/4.0/
  */
 package filededup;
-
-import java.io.File;
 
 /**
  * @author alb (Andrew Binstock)
@@ -23,7 +21,9 @@ public class Main {
      *    -help/-h which prints usage instructions
      */
     public static void main( final String[] args ) 
-    {  
+    {
+        printCopyright();
+        
         if( args.length == 0 ) {
             showUsage();
             return;
@@ -36,21 +36,31 @@ public class Main {
         }
     
         if( args.length == 1 ) {
-            DirDeduper dirDedupe = new DirDeduper( args[0] );
-            dirDedupe.go();
+            new DirDeduper( args[0] ).go();
         }
         else 
         if( args.length == 2 ) {
-            FilesDeduper filesDedupe = new FilesDeduper( args );
-            filesDedupe.go();
+            new FilesDeduper( args ).go();
         }
         else {
             System.out.println( "Invalid command. This might help:" );
             showUsage();
         }
     }
-        
-    static void showUsage() {
-        System.out.println( "Usage explanation to go here" ); //TODO: add text
+
+    private static void printCopyright()
+    {
+        System.out.println(
+            "FileDedupe v.0.1 (c) Copyright 2017, Andrew Binstock. All rights reserved.\n" );
+    }
+
+    private static void showUsage() {
+
+        System.out.println(
+            "FileDedupe finds duplicate files in a diretory or disk drive.\n" +
+            "arguments: 1 argument is the directory to process (which includes all subdirectories);\n" +
+            "           2 arguments are two files to compare to evaluate whether they're duplicates\n" +
+            "           -h or -help prints this message"
+        );
     }
 }
